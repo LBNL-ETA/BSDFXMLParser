584ad8f23a0cec6e947d8f05725f036e122af21b
Simon Vidanovic <DVVidanovic@lbl.gov>
2025-01-23 14:34:12 -0800

    Added function that will load window object from std::string.

--- src/bsdfdata/Parser.hpp
+++ src/bsdfdata/Parser.hpp
@@ -8,5 +8,6 @@
 namespace BSDFData
 {
     std::optional<WindowElement> loadWindowElementFromFile(std::string_view fileName);
+    std::optional<WindowElement> loadWindowElementFromString(const std::string & data);
     int saveToFile(const WindowElement & model, std::string_view fileName);
 }
